d4486fdf787e8c7028e619c630ae4de57a8f9cab
Alice Vee <auvalice@outlook.com>
2022-04-14 07:12:02 +0200

    Initial vitesse version

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+useHead({
+  title: 'Gear',
+  meta: [
+    { name: 'description', content: 'BrackeysBot plugin configurator' },
+  ],
+})
+</script>
+
+<template>
+  <RouterView />
+</template>
---
+++ src/pages/main.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const { t } = useI18n()
+</script>
+
+<template>
+  <div>
+    <div text-4xl>
+      <div i-carbon-settings inline-block w-32 h-32 />
+    </div>
+    <p>
+      <a rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank">
+        Gear
+      </a>
+    </p>
+    <p>
+      <em text-sm opacity-75>{{ t('app.description') }}</em>
+    </p>
+  </div>
+</template>
+
+<route lang="yaml">
+meta:
+  layout: home
+</route>
